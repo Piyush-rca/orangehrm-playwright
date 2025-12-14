bb1ed7674d7052ec330cc15dbb37d1a15bfb068a
Piyush Mangal <piyush.grn@gmail.com>
2025-12-14 17:22:38 +0530

    Update AppConstants.java

--- src/main/java/com/qa/orangeHRM/constants/AppConstants.java
+++ src/main/java/com/qa/orangeHRM/constants/AppConstants.java
@@ -3,7 +3,7 @@ package com.qa.orangeHRM.constants;
 public class AppConstants {
 	
 	public static final String LOGIN_PAGE_TITLE = "OrangeHRM";
-	public static final String DASHBOARD_PAGE_TITLE = "New Orbit";
+	
 	public static final String INVALID_LOGIN_ERROR = "Invalid credentials";
 	public static final String REQUIRED_FIELD_ERROR = "Required";
 	
